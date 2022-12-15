8e4d700563e3a95c8b2902144fcc70c3c888e8d6
Kailar0000 <kailar0000@gmail.com>
2022-12-15 13:15:38 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+int N = number.Length;
+int sum = 0;
+int a = 0;
+
+for(int i = 0; i < N; i++)
+{
+		a = Convert.ToInt32(number[i].ToString());
+		sum = sum + a;
+}
+Console.Write(sum);
