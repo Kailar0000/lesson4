a5dff9768b130c3e930ec97cb086d118a4b109bd
Kailar0000 <kailar0000@gmail.com>
2022-12-15 13:12:39 +0300

    Task25

---
+++ task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int pow = a;
+for(int i = 1; i < b; i++)
+	{
+		pow = pow * a;
+	}
+Console.Write(pow);
